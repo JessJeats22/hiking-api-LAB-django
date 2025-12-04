da3b34441f236e100009c502cbf9d10fe850164e
JessieJ <jvwiltshire22@gmail.com>
2025-12-04 10:42:55 +0000

    commiting

--- trails/views.py
+++ trails/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Trail
 
 # Create your views here.
 
@@ -9,4 +10,6 @@ class TrailShowView(APIView):
     # INDEX ROUTE
     # The full endpoint for this route is GET /trails
     def get(self, request):
+        trails = Trail.ojects.all()
+        print (trails)
         return Response({ 'detail': 'HIT TRAIL INDEX ROUTE' })
