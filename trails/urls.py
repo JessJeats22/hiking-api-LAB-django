8385550d924e0b6dc7773a526d590a61e3420520
JessieJ <jvwiltshire22@gmail.com>
2025-12-07 16:18:45 +0000

    added in PUT, PATCH and DELETE

--- trails/urls.py
+++ trails/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from trails.views import TrailShowView
+from trails.views import TrailShowView, TrailDetailView
 
 # Every request that hits this router file, will already start with `/trails/`
 urlpatterns = [
-    path('', TrailShowView.as_view())
+    path('', TrailShowView.as_view()),
+    path('<int:pk>/', TrailDetailView.as_view())
 ]
--- trails/views.py
+++ trails/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Trail
+from .serializers.common import TrailSerializer
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
 
@@ -10,6 +12,58 @@ class TrailShowView(APIView):
     # INDEX ROUTE
     # The full endpoint for this route is GET /trails
     def get(self, request):
-        trails = Trail.ojects.all()
-        print (trails)
-        return Response({ 'detail': 'HIT TRAIL INDEX ROUTE' })
+        trails = Trail.objects.all()
+        serializer = TrailSerializer(trails, many=True)
+        return Response(serializer.data)
+
+    # POST ROUTE
+    # The full endpoint for this route is POST /trails
+    def post(self, request):
+        serializer = TrailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, 201)
+
+
+
+# URL: /trails/:pk
+class TrailDetailView(APIView):
+           
+
+# Get Trail Object
+        def get_trail(self, pk):
+            try:
+                return Trail.objects.get(pk=pk)
+            except Trail.DoesNotExist:
+                raise NotFound('Trail not found')
+
+ # SHOW Route
+        def get(self, request, pk):
+            trail = self.get_trail(pk)
+            serializer = TrailSerializer(trail)
+            return Response(serializer.data)
+            
+ # PUT Route
+        def put(self, request, pk):
+            trail = self.get_trail(pk)
+            serializer = TrailSerializer(instance=trail, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        
+            
+# PATCH Route
+        def patch(self, request, pk):
+            trail = self.get_trail(pk)
+            serializer = TrailSerializer(instance=trail, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        
+
+ # DELETE Route
+        def delete(self, request, pk):
+            trail = self.get_trail(pk)
+            trail.delete()
+            return Response(status=204)
+
